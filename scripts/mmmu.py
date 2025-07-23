d387154844727dc2d7d7dfe0fb836b31d3da9779
Ilija <ilija.lichkovski@gmail.com>
2025-07-23 18:14:35 +0000

    mmmu translation script working

--- scripts/mmmu.py
+++ scripts/mmmu.py
@@ -4,16 +4,16 @@ from datasets import load_dataset
 
 def translate_mmmu():
     mmmu_configs = ['Accounting', 'Agriculture'] #'Architecture_and_Engineering', 'Art', 'Art_Theory', 'Basic_Medical_Science', 'Biology', 'Chemistry', 'Clinical_Medicine', 'Computer_Science', 'Design', 'Diagnostics_and_Laboratory_Medicine', 'Economics', 'Electronics', 'Energy_and_Power', 'Finance', 'Geography', 'History', 'Literature', 'Manage', 'Marketing', 'Materials', 'Math', 'Mechanical_Engineering', 'Music', 'Pharmacy', 'Physics', 'Psychology', 'Public_Health', 'Sociology']
+    translator = LLMTranslator(
+    source_lang="en",
+    target_lang="mk",
+    backend="nllb",
+    model_name="facebook/nllb-200-3.3B",
+    max_gen_tokens=1024,
+    project_id="synglot-441912",
+    device="cuda"
+    )
     for config in mmmu_configs:
-        translator = LLMTranslator(
-            source_lang="en",
-            target_lang="mk",
-            backend="nllb",
-            model_name="facebook/nllb-200-3.3B",
-            max_gen_tokens=1024,
-            project_id="synglot-441912",
-            device="cuda"
-        )
         dataset = load_dataset("MMMU/MMMU", config, split="validation")
         result = translator.translate_dataset(
             dataset=dataset,
